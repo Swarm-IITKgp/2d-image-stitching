68a5a680abf529809e5f78bbba25c3e5cd307af3
SURYA S DWIVEDI <surya2191997@gmail.com>
2017-09-30 11:08:18 +0530

    Update stitch.cpp

--- stitch.cpp
+++ stitch.cpp
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <iostream>
-#include <stdio.h>
-#include <iostream>
 #include "opencv2/core/core.hpp"
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/highgui/highgui.hpp"
